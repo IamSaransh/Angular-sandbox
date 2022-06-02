bda82fdbc8f50c90ba0d606377afe83517a18c84
Saransh <saranshvashistha@gmail.com>
2022-06-03 00:23:17 +0530

    video 35

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
+  // styles: can also be added instead of styleUrls  simimalr to adding 
+  //template: instead of templetUrl
+  // both are inline
 })
 export class AppComponent {
 }
