1368b314f43cfc6561963ae66202340c4ec610ff
Saransh <saranshvashistha@gmail.com>
2022-07-01 12:16:06 +0530

    attribyte directive and Structural directuivs added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent } from './app.component';
-
+import {BaicHighLightDirective} from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight-directive/better-highlight.directive';
+import { UnlessDirective } from './unless.directive'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaicHighLightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
